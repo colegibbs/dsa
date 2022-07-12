48ad1419b3c06f0d1b863b8cf40fa74af40eee6d
Cole Gibbs <colegibbs0@gmail.com>
2022-07-12 15:56:04 -0700

    positives and negatives test

--- longest-consecutive-sequence/tests/test_solution.py
+++ longest-consecutive-sequence/tests/test_solution.py
@@ -15,3 +15,10 @@ def test_list_negatives():
   instantiate = LongestConsecutiveSequence()
   actual = instantiate.solution(list)
   assert expected == actual
+
+def test_list_positive_negative():
+  list = [-100, -4, -200, -1, -2, -3, 0, 1, 7, 9]
+  expected = 6
+  instantiate = LongestConsecutiveSequence()
+  actual = instantiate.solution(list)
+  assert expected == actual
