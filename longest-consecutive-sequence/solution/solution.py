b272aebb942f71d30a868ebce27a3cf61f112590
Cole Gibbs <colegibbs0@gmail.com>
2022-07-12 15:53:02 -0700

    corrected solution

---
+++ longest-consecutive-sequence/solution/solution.py
@@ -0,0 +1,18 @@
+class LongestConsecutiveSequence():
+  def __init__(self):
+    pass
+
+  def solution(self, nums):
+    # [100, 4, 200, 1, 2, 3]
+    greatest = 1
+    for item in nums:
+      current = 1 
+      while (item + 1) in nums:
+        current += 1
+        item += 1
+
+      if current > greatest:
+        greatest = current
+    
+    return greatest
+
