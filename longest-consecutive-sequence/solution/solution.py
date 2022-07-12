5912a9d8fb8dee28db4554ade6b32f2068ed4308
Cole Gibbs <colegibbs0@gmail.com>
2022-07-12 16:03:58 -0700

    empty list error handling

--- longest-consecutive-sequence/solution/solution.py
+++ longest-consecutive-sequence/solution/solution.py
@@ -4,6 +4,9 @@ class LongestConsecutiveSequence():
 
   def solution(self, nums):
     # [100, 4, 200, 1, 2, 3]
+    if nums == []:
+      raise ValueError("List must contain at least one numeric value")
+
     greatest = 1
     for item in nums:
       current = 1 
