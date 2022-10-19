90398e2c97b2aba0ded5833fb77347d486623f29
Cole Gibbs <colegibbs0@gmail.com>
2022-10-19 13:14:13 -0700

    height_checker solutin

---
+++ height-checker/height_checker.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+
+        def partition(list, low, high):
+            pivot = list[high]
+            i = low - 1
+            for j in range(low, high):
+                if list[j] <= pivot:
+                    i += 1
+                    list[i], list[j] = list[j], list[i]
+            list[i + 1], list[high] = list[high], list[i + 1]
+            return i + 1
+
+        def quickSort(list, low, high):
+            if low < high:
+                part_index = partition(list, low, high)
+                quickSort(list, low, part_index - 1)
+                quickSort(list, part_index + 1, high)
+
+        size = len(heights)
+        expected_heights = heights[:]
+        quickSort(expected_heights, 0, size - 1)
+        desc = 0
+        for i in range(size):
+            if heights[i] != expected_heights[i]:
+                desc += 1
+        return desc
