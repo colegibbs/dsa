7dbe476e7d6487ced6cd17838f8b89bcf30dd687
Cole Gibbs <colegibbs0@gmail.com>
2022-11-09 14:33:44 -0800

    edits

--- add-two-numbers/solution.py
+++ add-two-numbers/solution.py
@@ -4,7 +4,8 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-class Solution(object):
+
+class Solution:
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
@@ -13,7 +14,7 @@ class Solution(object):
         """
         dummyHead = ListNode(0)
         current = dummyHead
-        carry = 0 
+        carry = 0
         while l1 or l2 or carry != 0:
             value_one = l1.val if l1 else 0
             value_two = l2.val if l2 else 0
@@ -21,16 +22,12 @@ class Solution(object):
             if carry == 1:
                 carry = 0
             if sum > 9:
-                sum = sum % 10 
+                sum = sum % 10
                 carry = 1
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
             newNode = ListNode(sum)
             current.next = newNode
             current = current.next
-            
-                
+
         return dummyHead.next
-            
-                
-        
